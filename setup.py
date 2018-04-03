54a8ad62ca172ad11673324b4423577aa386feac
Alexandre René <alexandre.rene@freeneurons.ca>
2018-04-03 20:55:01 +0200

    [setup] Add tqdm dependency.

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
 
       install_requires=['numpy >= 1.13',
                         'scipy',
-                        'dill']
+                        'dill',
+                        'tqdm']
       # Add mackelab, theano as optional dependency
      )
