14ec2cfc6955b7565e8b18bc3ad573518a673564
Alexandre René <alexandre.rene@freeneurons.ca>
2018-08-28 20:37:30 +0200

    [models] Drift-diffusion model works correctly.

--- sinn/models/driftdiffusion.py
+++ sinn/models/driftdiffusion.py
@@ -14,6 +14,7 @@ import logging
 
 import theano_shim as shim
 import sinn
+from sinn.histories import Series
 from sinn.models.common import Model, register_model
 
 # TODO: Package simpleeval boilerplate in mackelab.utils
@@ -82,8 +83,8 @@ class DriftDiffusion(Model):
     Parameters = sinn.define_parameters(Parameter_info)
     State = namedtuple('State', ['x'])
 
-    def __init__(self, params, x_history,
-                 drift=None, diffusion=None, random_stream=None):
+    def __init__(self, params, x_history, random_stream=None,
+                 drift=None, diffusion=None, namespace=None):
         """
         Default `drift` and `diffusion` implement
             `dx = -x dt + dW`
@@ -91,24 +92,30 @@ class DriftDiffusion(Model):
         Parameters
         ----------
         drift: function (t, x) -> floatX (shape: x_history.shape)
-           Function which takes a time and history object, and returns
-           the drift. Should output the same shape as the history.
+            Function which takes a time and history object, and returns
+            the drift. Should output the same shape as the history.
         diffusion: function (t, x) -> floatX (shape: x_history.shape)
-           Function which takes a time and history object, and returns
-           the diffusion. Should output the same shape as the history.
+            Function which takes a time and history object, and returns
+            the diffusion. Should output the same shape as the history.
+        namespace: dict
+            If either `drift` or `diffusion` are string expressions,
+            `namespace` is added to their SimpleEval namespaces. Allows to
+            define parameters which can be used in the string expressions.
         """
 
-        if drift is not None: self.drift = drift
-        if diffusion is not None: self.diffusion = diffusion
+        #if drift is not None: self.drift = drift
+        #if diffusion is not None: self.diffusion = diffusion
         self.rndstream = random_stream
+
+        # Argument tests
+        if not isinstance(x_history, Series):
+            raise TypeError("`x_history` argument must be a sinn `Series`.")
+        Model.output_rng(x_history, self.rndstream)
 
         super().__init__(params, public_histories=(x_history,))
         # NOTE: Do not use `params` beyond here. Always use self.params.
 
         self.x = x_history
-
-        # Parameter tests
-        Model.output_rng(self.x, self.rndstream)
 
         self.add_history(self.x)
         self.x.set_update_function(self.x_fn)
@@ -117,6 +124,8 @@ class DriftDiffusion(Model):
         if isinstance(drift, str):
             # Evaluate string to get function
             self.drift = StringFunction(drift, ('t', 'x'))
+            if namespace is not None:
+                self.drift.namespaces.update(namespace)
         elif drift is not None:
             # Just assign whatever was pased.
             assert(isinstance(drift, Callable))
@@ -124,15 +133,44 @@ class DriftDiffusion(Model):
         if isinstance(diffusion, str):
             # Evaluate string to get function
             self.diffusion = StringFunction(diffusion, ('t', 'x'))
+            if namespace is not None:
+                self.diffusion.namespaces.update(namespace)
         elif diffusion is not None:
             # Just assign whatever was pased.
             assert(isinstance(diffusion, Callable))
             self.diffusion = diffusion
 
+    def initialize(self, init_cond=None):
+        if init_cond is not None:
+            # Initialize the history
+            init = init_cond['x']
+            if init.shape == self.x.shape:
+                # Add time dimension
+                init = init.reshape((1,) + self.x.shape)
+            elif not init.shape[1:] == shape:
+                raise ValueError("`init_cond` (shape: {}) doesn't match "
+                                 "the expected shape ({})."
+                                 .format(init.shape, self.x.shape))
+            self.x.pad(len(init))
+            self.x[:len(init)] = init
+
+
     def x_fn(self, t):
         t = self.x.get_tidx(t)
-        return (self.drift(t, self.x) * self.x.dt
-                + self.diffusion(t, self.x) * np.sqrt(self.x.dt))
+        return self.x[t-1] + (self.drift(t, self.x) * self.x.dt
+                              + self.diffusion(t, self.x) * self.dW())
+
+    def dW(self):
+        """
+        Returns a step from a Wiener process. Specifically, samples from
+        .. math::
+          dW \sim \mathcal{N}(0, dt^2)
+        """
+        # TODO: Allow noise to have different shape from X
+        # TODO: Preevaluate arguments ? Something like
+        #      __init__: `sampler = rndstream(*args)`
+        #      here:     `sampler()`
+        return self.rndstream.normal(size=self.x.shape, avg=0, std=np.sqrt(self.x.dt))
 
     def drift(self, t):
         tidx = x.get_t_idx(t)
