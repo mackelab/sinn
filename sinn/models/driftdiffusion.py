76656f2dd6532ee07ca89168662db2a5b6892c0a
Alexandre René <alexandre.rene@freeneurons.ca>
2018-09-17 00:08:10 +0200

    [models:DD] Move StringFunction to mackelab.utils

--- sinn/models/driftdiffusion.py
+++ sinn/models/driftdiffusion.py
@@ -17,60 +17,7 @@ import sinn
 from sinn.histories import Series
 from sinn.models.common import Model, register_model
 
-# TODO: Package simpleeval boilerplate in mackelab.utils
-import simpleeval
-import ast
-import operator
-
-# TODO: Find a way to pre-evaluate strings to some more efficient expresison, so
-#       we don't need to parse the string every time.
-class StringFunction:
-    # Replace the "safe" operators with their standard forms
-    # (simpleeval implements safe_add, safe_mult, safe_exp, which test their
-    #  input but this does not work with non-numerical types.)
-    _operators = simpleeval.DEFAULT_OPERATORS
-    _operators.update(
-        {ast.Add: operator.add,
-         ast.Mult: operator.mul,
-         ast.Pow: operator.pow})
-    # Allow evaluation to find operations in standard namespaces
-    namespaces = {'np': np,
-                  'sp': sp}
-
-    def __init__(self, expr, args):
-        """
-        Parameters
-        ----------
-        expr: str
-            String to evaluate.
-        args: iterable of strings
-            The function argument names.
-        """
-        self.expr = expr
-        self.args = args
-    def __call__(self, *args, **kwargs):
-        names = {nm: arg for nm, arg in zip(self.args, args)}
-        names.update(kwargs)  # FIXME: Unrecognized args ?
-        names.update(self.namespaces)  # FIXME: Overwriting of arguments ?
-        try:
-            res = simpleeval.simple_eval(
-                self.expr,
-                operators=self._operators,
-                names=names)
-        except simpleeval.NameNotDefined as e:
-            e.args = ((e.args[0] +
-                       "\n\nThis may be due to a module function in the transform "
-                       "expression (only numpy and scipy, as 'np' and 'sp', are "
-                       "available by default).\nIf '{}' is a module or class, you can "
-                       "make it available by adding it to the function namespace: "
-                       "`StringFunction.namespaces.update({{'{}': {}}})`.\nSuch a line would "
-                       "typically be included at the beginning of the execution script "
-                       "(it does not need to be in the same module as the one where "
-                       "the string function is defined, as long as it is executed before)."
-                       .format(e.name, e.name, e.name),)
-                      + e.args[1:])
-            raise
-        return res
+from mackelab.utils import StringFunction
 
 class DriftDiffusion(Model):
     """
