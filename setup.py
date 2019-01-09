dbb6a442d843d50af303969249a62e432af841cc
Alexandre René <alexandre.rene@freeneurons.ca>
2019-01-08 19:05:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,27 +3,30 @@ from setuptools import setup
 
 setup(
       name='sinn',
-      version='0.1.dev1',
+      version='0.1.0dev',
       description="A package for Simulation and Inference of Neuron Networks",
 
       author="Alexandre René",
-      author_email="alexandre.rene@caesar.de",
+      author_email="arene010@uottawa.ca",
 
       license='MIT',
 
       classifiers=[
-          'Development Status :: 2 - Pre-Alpha',
+          'Development Status :: 3 - Alpha',
           'Intended Audience :: Science/Research',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3 :: Only'
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Scientific/Engineering :: Information Analysis'
       ],
 
       packages=['sinn'],
 
       install_requires=['numpy >= 1.13',
                         'scipy',
-                        'dill',
+                        #'dill', # Histories are picklable now
                         'tqdm',
                         'packaging']
       # Add mackelab, theano as optional dependency
+      # luigi is not included, since nothing imports luigi.py
+      # A user importing sinn.models.luigi should expect to install luigi
      )
