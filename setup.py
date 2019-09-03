11e50760fbc21efcaab7c88961a1f2a38820ce81
Alexandre René <alexandre.rene@freeneurons.ca>
2019-09-03 12:28:05 +0200

    Set version number to 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
       name='sinn',
-      version='0.1.0dev',
+      version='0.1.0',
       description="A package for Simulation and Inference of Neuron Networks",
 
       author="Alexandre René",
