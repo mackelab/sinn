e16132ac8953961611ac3df06f3e6b25aa57c694
Alexandre René <alexandre.rene@freeneurons.ca>
2020-11-10 17:14:52 +0100

    Move `unlocked_hists` to `utils` subpackage
    It used to be in histories.py with a note “move to utils if such a 
    module is created”.
    Since I couldn't find it myself without a project-wide search, and I 
    explicitely looked for a 'utils' module first, this move definitely 
    makes it more discoverable.

---
+++ sinn/utils/__init__.py
@@ -0,0 +1,20 @@
+########################
+# Convenience methods
+# (TODO: Move to utils, if such a module is created)
+
+class unlocked_hists:
+    """
+    Context manager, for temporarily ensuring that a set of histories
+    are unlocked, without changing their state permanently.
+    """
+    def __init__(self, *hists):
+        self.hists = hists
+        self.lock_states = None
+    def __enter__(self):
+        self.lock_states = {h: h.locked for h in self.hists}
+        for h in self.hists:
+            h.unlock()
+    def __exit__(self, exc_type, exc_value, traceback):
+        for h, locked in self.lock_states.items():
+            if locked:
+                h.lock(warn=False)
