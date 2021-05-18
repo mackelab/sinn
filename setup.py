1089fec9ce98b4cae127b45033d0234abe8e4083
Alexandre René <alexandre.rene@freeneurons.ca>
2021-05-18 17:57:50 +0200

    Add 'parameters' dependency to setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
       install_requires=['numpy >= 1.13',
                         'scipy',
                         'pydantic >= 1.7.1',
+                        'parameters',
                         'mackelab-toolbox[iotools,utils,typing,parameters,theano] >= 0.2.0a1',
                         'theano_shim >= 0.2.3',
                         'theano-pymc',
