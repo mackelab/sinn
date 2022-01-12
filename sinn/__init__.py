a8228da802623c0851c41cdb7a4220473d5a1868
Alexandre René <alexandre.rene@freeneurons.ca>
2022-01-12 17:54:43 +0100

    Better way to retrieve __version__

--- sinn/__init__.py
+++ sinn/__init__.py
@@ -5,9 +5,9 @@ from .common import *
 from .histories import TimeAxis, History
 from .models import Model, ModelParams
 
-# Set __version__
-import pkg_resources
-__version__ = pkg_resources.require("sinn")[0].version
+# Set __version__ – See https://packaging.python.org/en/latest/guides/single-sourcing-package-version/ technique #5
+from importlib import metadata
+__version__ = metadata.version('sinn')
 config.compat_version = __version__
     # If some behaviour changes with a particular version,
-    # 'compate_version' can be set to recover the old behaviour.
+    # 'compat_version' can be set to recover the old behaviour.
